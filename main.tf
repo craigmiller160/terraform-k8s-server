66bd84d28494ae1b785af1a77f292d204eb75f28
Craig Miller <craigmiller160@gmail.com>
2022-07-31 10:38:57 -0400

    Linting

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ provider "kubernetes" {
 }
 
 module "databases" {
-  source = "./modules/databases"
+  source                 = "./modules/databases"
   postgres_root_password = var.postgres_root_password
-  mongodb_root_password = var.mongodb_root_password
+  mongodb_root_password  = var.mongodb_root_password
 }
