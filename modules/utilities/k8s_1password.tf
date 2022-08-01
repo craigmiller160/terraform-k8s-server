a066a923e0246b32b4dd33d35c8d97a70f37a3c4
Craig Miller <craigmiller160@gmail.com>
2022-08-01 19:14:35 -0400

    Working on 1password config

---
+++ modules/utilities/k8s_1password.tf
@@ -0,0 +1,42 @@
+
+# TODO need TLS for this
+
+resource "kubernetes_deployment" "onepassword_connect_server" {
+  metadata {
+    name = "1password"
+  }
+  spec {
+    revision_history_limit = 0
+    replicas = 1
+    selector {
+      match_labels = {
+        app = "1password"
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          app = "1password"
+        }
+      }
+      spec {
+        container {
+          name = "1password"
+          image = "1password/connect-api:1.5"
+          image_pull_policy = "IfNotPresent"
+          port {
+            container_port = 8080
+          }
+          volume_mount {
+            mount_path = "/home/opuser/.op/data"
+            name       = "1password-volume"
+          }
+        }
+        volume {
+          name = "1password-volume"
+          empty_dir {}
+        }
+      }
+    }
+  }
+}
