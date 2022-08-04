4cc49afc4eae2abc514c994a32cb3caf678301a6
Craig Miller <craigmiller160@gmail.com>
2022-08-03 21:44:28 -0400

    Continuing to work on refactor

--- dev.sh
+++ dev.sh
@@ -5,7 +5,7 @@ if [ $1 == "init" ]; then
   backend_arg = "-backend-config='config_context=kind'"
 fi
 
-TF_LOG=DEBUG terraform $@ \
+terraform $@ \
   -var="k8s_context=kind" \
   -var-file="secrets.tfvars" \
   $backend_arg
