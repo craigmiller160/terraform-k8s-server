040bb5b1d56b24f1e7e703193d04a7a67eb4af28
Craig Miller <craigmiller160@gmail.com>
2022-08-04 08:10:38 -0400

    More work on 1password provided docs

--- modules/utilities/k8s_1password_provided.tf
+++ modules/utilities/k8s_1password_provided.tf
@@ -5,12 +5,22 @@
 locals {
   all_service_account_docs = split("---", file("${path.module}/k8s_yaml/1password_provided/1password_service_account.yml"))
   service_account_doc = local.all_service_account_docs.0
+  cluster_role_binding_doc = local.all_service_account_docs.1
+  cluster_role_doc = local.all_service_account_docs.2
 }
 
-#resource "kubernetes_manifest" "onepassword_provided_item" {
-#  manifest = yamldecode(file("${path.module}/k8s_yaml/1password_provided/1password_item.yml"))
-#}
+resource "kubernetes_manifest" "onepassword_provided_item" {
+  manifest = yamldecode(file("${path.module}/k8s_yaml/1password_provided/1password_item.yml"))
+}
 
 resource "kubernetes_manifest" "onepassword_provided_service_account" {
   manifest = yamldecode(local.service_account_doc)
+}
+
+resource "kubernetes_manifest" "onepassword_provided_cluster_role_binding" {
+  manifest = yamldecode(local.cluster_role_binding_doc)
+}
+
+resource "kubernetes_manifest" "onepassword_provided_cluster_role" {
+  manifest = yamldecode(local.cluster_role_doc)
 }
