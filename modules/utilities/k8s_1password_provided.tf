3b24eac568f86c3298c3c99987c9eb3d56922079
Craig Miller <craigmiller160@gmail.com>
2022-08-03 21:50:09 -0400

    Got custom resources working

--- modules/utilities/k8s_1password_provided.tf
+++ modules/utilities/k8s_1password_provided.tf
@@ -1,6 +1,6 @@
-#resource "kubernetes_manifest" "onepassword_provided_item" {
-#  manifest = yamldecode(file("${path.module}/k8s_yaml/1password_provided/1password_item.yml"))
-#}
+resource "kubernetes_manifest" "onepassword_provided_item" {
+  manifest = yamldecode(file("${path.module}/k8s_yaml/1password_provided/1password_item.yml"))
+}
 
 resource "kubernetes_manifest" "onepassword_provided_service_account" {
   manifest = yamldecode(file("${path.module}/k8s_yaml/1password_provided/1password_service_account.yml"))
