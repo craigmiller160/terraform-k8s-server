95147785adae0f484581e3ac5c0563820cd5d7ea
Craig Miller <craigmiller160@gmail.com>
2022-08-04 15:40:18 -0400

    Working on dynamic image support

--- infrastructure/modules/utilities/k8s_nexus.tf
+++ infrastructure/modules/utilities/k8s_nexus.tf
@@ -1,5 +1,7 @@
 locals {
-  nexus_all_docs = split("---", file("${path.module}/k8s_yaml/nexus.yml"))
+  nexus_all_docs = split("---",
+    replace(file("${path.module}/k8s_yaml/nexus.yml"), "%NEXUS_IMAGE%", "klo2k/nexus3:3.29.2-02")
+  )
   nexus_deployment_doc = local.nexus_all_docs.0
   nexus_service_doc = local.nexus_all_docs.1
 }
