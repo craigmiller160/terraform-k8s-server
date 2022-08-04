8e0e8fc2087794168c40c1eee6c85ef487fd9586
Craig Miller <craigmiller160@gmail.com>
2022-08-04 08:08:59 -0400

    More progress with provided docs

--- modules/utilities/k8s_1password_provided.tf
+++ modules/utilities/k8s_1password_provided.tf
@@ -1,11 +1,16 @@
 #data "onepassword_service_account" "documents" {
-#  content = split(file("${path.module}/k8s_yaml/1password_provided/1password_service_account.yml"))
+#  content = split()
 #}
+
+locals {
+  all_service_account_docs = split("---", file("${path.module}/k8s_yaml/1password_provided/1password_service_account.yml"))
+  service_account_doc = local.all_service_account_docs.0
+}
 
 #resource "kubernetes_manifest" "onepassword_provided_item" {
 #  manifest = yamldecode(file("${path.module}/k8s_yaml/1password_provided/1password_item.yml"))
 #}
 
-#resource "kubernetes_manifest" "onepassword_provided_service_account" {
-#  manifest = yamldecode(data.onepassword_service_account_documents)
-#}
+resource "kubernetes_manifest" "onepassword_provided_service_account" {
+  manifest = yamldecode(local.service_account_doc)
+}
