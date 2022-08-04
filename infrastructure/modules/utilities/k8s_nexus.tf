41288f9108eea0a0ed563226a417711787161752
Craig Miller <craigmiller160@gmail.com>
2022-08-04 14:45:06 -0400

    Setup deployment and service for nexus

--- infrastructure/modules/utilities/k8s_nexus.tf
+++ infrastructure/modules/utilities/k8s_nexus.tf
@@ -3,3 +3,11 @@ locals {
   nexus_deployment_doc = local.nexus_all_docs.0
   nexus_service_doc = local.nexus_all_docs.1
 }
+
+resource "kubernetes_manifest" "nexus_deployment" {
+  manifest = yamldecode(local.nexus_deployment_doc)
+}
+
+resource "kubernetes_manifest" "nexus_service" {
+  manifest = yamldecode(local.nexus_service_doc)
+}
