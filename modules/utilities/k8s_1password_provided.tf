44c18dbcf6f0aa975ad48cd1347942399494c3d2
Craig Miller <craigmiller160@gmail.com>
2022-08-03 21:56:40 -0400

    Trying to make progress

--- modules/utilities/k8s_1password_provided.tf
+++ modules/utilities/k8s_1password_provided.tf
@@ -1,7 +1,13 @@
+data "onepassword_service_account" "documents" {
+  abc = "def"
+  content = abc
+#  content = split(file("${path.module}/k8s_yaml/1password_provided/1password_service_account.yml"))
+}
+
 resource "kubernetes_manifest" "onepassword_provided_item" {
   manifest = yamldecode(file("${path.module}/k8s_yaml/1password_provided/1password_item.yml"))
 }
 
 resource "kubernetes_manifest" "onepassword_provided_service_account" {
-  manifest = yamldecode(file("${path.module}/k8s_yaml/1password_provided/1password_service_account.yml"))
+  manifest = yamldecode(data.onepassword_service_account_documents)
 }
