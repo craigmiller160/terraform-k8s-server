3a7e241e0e19de048dd0bd8fff3c34989eea1f50
Craig Miller <craigmiller160@gmail.com>
2022-08-07 21:06:25 -0400

    Beginning experiment to figure out docker piece

--- infrastructure/modules/utilities/k8s_nexus.tf
+++ infrastructure/modules/utilities/k8s_nexus.tf
@@ -6,19 +6,27 @@ locals {
   nexus_service_doc = local.nexus_all_docs.1
 }
 
-resource "docker_registry_image" "extended_busybox" {
-  name = "localhost:5000/extended-busybox:1.1"
+#resource "docker_registry_image" "extended_busybox" {
+#  name = "localhost:5000/extended-busybox:1.1"
+#  build {
+#    context = "${path.module}/docker"
+#    dockerfile = "ExtendedBusyBox_Dockerfile"
+#  }
+#}
+
+resource "docker_image" "experiment" {
+  name = "experiment"
   build {
-    context = "${path.module}/docker"
+    path = "${path.module}/docker"
     dockerfile = "ExtendedBusyBox_Dockerfile"
   }
 }
 
-resource "kubernetes_manifest" "nexus_deployment" {
-  depends_on = [docker_registry_image.extended_busybox]
-  manifest = yamldecode(local.nexus_deployment_doc)
-}
-
-resource "kubernetes_manifest" "nexus_service" {
-  manifest = yamldecode(local.nexus_service_doc)
-}
+#resource "kubernetes_manifest" "nexus_deployment" {
+#  depends_on = [docker_registry_image.extended_busybox]
+#  manifest = yamldecode(local.nexus_deployment_doc)
+#}
+#
+#resource "kubernetes_manifest" "nexus_service" {
+#  manifest = yamldecode(local.nexus_service_doc)
+#}
