a7842e6dd96158a7ccd1cee89a81340c626f823e
Craig Miller <craigmiller160@gmail.com>
2022-08-08 18:44:07 -0400

    Added nexus configmap

--- infrastructure/modules/utilities/k8s_nexus.tf
+++ infrastructure/modules/utilities/k8s_nexus.tf
@@ -2,8 +2,13 @@ locals {
   nexus_all_docs = split("---",
     replace(file("${path.module}/k8s_yaml/nexus.yml"), "%NEXUS_IMAGE%", var.nexus_image)
   )
-  nexus_deployment_doc = local.nexus_all_docs.0
-  nexus_service_doc = local.nexus_all_docs.1
+  nexus_config_doc = local.nexus_all_docs.0
+  nexus_deployment_doc = local.nexus_all_docs.1
+  nexus_service_doc = local.nexus_all_docs.2
+}
+
+resource "kubernetes_manifest" "nexus_config" {
+  manifest = yamldecode(local.nexus_config_doc)
 }
 
 resource "kubernetes_manifest" "nexus_deployment" {
