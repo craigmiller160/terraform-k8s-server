becddb2e865375b500ac2300b73b790ccb247958
Craig Miller <craigmiller160@gmail.com>
2022-08-07 21:25:09 -0400

    Updated k8s script to properly create p12

--- infrastructure/modules/utilities/k8s_nexus.tf
+++ infrastructure/modules/utilities/k8s_nexus.tf
@@ -6,16 +6,7 @@ locals {
   nexus_service_doc = local.nexus_all_docs.1
 }
 
-resource "docker_registry_image" "extended_busybox" {
-  name = "localhost:5000/extended-busybox:1.1"
-  build {
-    context = "${path.module}/docker"
-    dockerfile = "ExtendedBusyBox_Dockerfile"
-  }
-}
-
 resource "kubernetes_manifest" "nexus_deployment" {
-  depends_on = [docker_registry_image.extended_busybox]
   manifest = yamldecode(local.nexus_deployment_doc)
 }
 
