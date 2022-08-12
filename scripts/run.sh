5d7f6680dd6c6d20301ad92106ce0ad9c7e3844d
Craig Miller <craigmiller160@gmail.com>
2022-08-12 13:25:52 -0400

    More progress with scripts

--- scripts/run.sh
+++ scripts/run.sh
@@ -11,6 +11,18 @@ function get_env {
   esac
 }
 
+# $1 = Directory
+function validate_phase {
+  case $1 in
+    "phase1"|"phase2"|"phase3")
+      echo "Infrastructure phase: $1"
+    *)
+      echo "Invalid infrastructure phase: $1"
+      exit 1
+    ;;
+  esac
+}
+
 # $1 = Command
 function get_backend_context {
   if [ $1 == "init" ]; then
@@ -33,13 +45,17 @@ function get_secrets_file {
 function get_nexus_image_var {
   if [ $1 == "phase2" ]; then
     echo "-var=nexus_image=$nexus_image"
+  else
+    echo ""
+  fi
 }
 
 # $1 = env, $2 = Directory, #3 = Command
-function run {
+export function run {
+  get_env $1
+  validate_phase $2
+
   backend_arg=$(get_backend_context $3)
   secrets_file=$(get_secrets_file $2)
   nexus_image_var=$(get_nexus_image_var $2)
-
-  get_env $1
 }
--- tf.sh
+++ tf.sh
@@ -1,19 +1,10 @@
 #!/bin/bash
 
-dev_nexus_image="klo2k/nexus3:3.29.2-02"
-prod_nexus_image="sonatype/nexus3:3.29.2"
+source ./scripts/run.sh
 
-function parse_args {
-  if [ $# -lt 2 ]; then
-    echo "Must specify environment & terraform command"
-    exit 1
-  fi
+if [ $# -lt 3 ]; then
+  echo "Must specify environment, phase, & terraform command"
+  exit 1
+fi
 
-  # TODO finish this
-  case $1 in
-    "phase1"|"phase2"|"phase3") ;;
-    *) ;;
-  esac
-}
-
-parse_args $@
+run $@
