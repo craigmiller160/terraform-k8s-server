323511086d1aec95fdb0e888ccaefcfc0016dc73
Craig Miller <craigmiller160@gmail.com>
2022-08-12 14:23:19 -0400

    Added blobs for docker

--- phase3/blobs.tf
+++ phase3/blobs.tf
@@ -12,3 +12,18 @@ resource "nexus_blobstore_file" "npm_group" {
   name = "npm-group"
   path = "/nexus-data/npm-group"
 }
+
+resource "nexus_blobstore_file" "docker_private" {
+  name = "docker-private"
+  path = "/nexus-data/docker-private"
+}
+
+resource "nexus_blobstore_file" "docker_proxy" {
+  name = "docker-proxy"
+  path = "/nexus-data/docker-proxy"
+}
+
+resource "nexus_blobstore_file" "docker_group" {
+  name = "docker-group"
+  path = "/nexus-data/docker-group"
+}
