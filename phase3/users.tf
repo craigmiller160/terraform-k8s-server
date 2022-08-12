159b984860f1f91a356a2eda4e87a50c8c0765c4
Craig Miller <craigmiller160@gmail.com>
2022-08-12 13:54:31 -0400

    Updated users config

---
+++ phase3/users.tf
@@ -0,0 +1,9 @@
+resource "nexus_security_user" "craigmiller160" {
+  userid = "craigmiller160"
+  firstname = "Craig"
+  lastname = "Miller"
+  email = "craigmiller160@gmail.com"
+  password = var.nexus_craig_password
+#  roles = ["nx-admin"]
+  status = "active"
+}
--- phase3/variables.tf
+++ phase3/variables.tf
@@ -1,3 +1,7 @@
 variable "nexus_admin_password" {
   type = string
 }
+
+variable "nexus_craig_password" {
+  type = string
+}
