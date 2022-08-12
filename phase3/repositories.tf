1fe1c18d6377853c6f84b8e201b7c7b9dfc17748
Craig Miller <craigmiller160@gmail.com>
2022-08-12 14:10:25 -0400

    Added another npm repo

--- phase3/repositories.tf
+++ phase3/repositories.tf
@@ -12,3 +12,24 @@ resource "nexus_repository_npm_hosted" "npm_private" {
     write_policy = "ALLOW_ONCE"
   }
 }
+
+resource "nexus_repository_npm_proxy" "npm_proxy" {
+  name = "npm-proxy"
+  online = true
+
+  storage {
+    blob_store_name = nexus_blobstore_file.npm_private.name
+    strict_content_type_validation = true
+  }
+
+  proxy {
+    remote_url = "https://registry.npmjs.org"
+    content_max_age = 1440
+    metadata_max_age = 1440
+  }
+
+  negative_cache {
+    enabled = true
+    time_to_live = 1440
+  }
+}
