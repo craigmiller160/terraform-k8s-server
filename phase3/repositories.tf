f5af4ef39e758502efe54e6352bc5325fd272f1b
Craig Miller <craigmiller160@gmail.com>
2022-08-12 14:06:07 -0400

    Created first repostiory

---
+++ phase3/repositories.tf
@@ -0,0 +1,13 @@
+# Private NPM Repository (Hosted)
+# NPM Proxy Repository
+# NPM Repository Group
+
+resource "nexus_repository_npm_hosted" "npm_private" {
+  name = "npm-private"
+  online = true
+
+  storage {
+    blob_store_name = nexus_blobstore_file.npm_private.name
+    strict_content_type_validation = true
+  }
+}
