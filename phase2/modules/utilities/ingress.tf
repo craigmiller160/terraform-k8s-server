8205515ed830b40299e69919dd70ba295b4fb5da
Craig Miller <craigmiller160@gmail.com>
2022-08-14 19:57:06 -0400

    Trying to make some progress

--- phase2/modules/utilities/ingress.tf
+++ phase2/modules/utilities/ingress.tf
@@ -5,15 +5,26 @@
 #  spec {}
 #}
 
-resource "kubernetes_config_map" "nginx_load_balancer_microk8s_conf" {
-  metadata {
-    name = "nginx-load-balancer-microk8s-conf"
-    namespace = "ingress"
-    annotations = {
-      kubectl.kubernetes.io/last-applied-configuration = "{\"apiVersion\":\"v1\",\"kind\":\"ConfigMap\",\"metadata\":{\"annotations\":{},\"name\":\"nginx-load-balancer-microk8s-conf\",\"namespace\":\"ingress\"}}"
-    }
-  }
-  data = {
-    use-forwarded-headers = "true"
-  }
-}
+#resource "kubernetes_config_map" "nginx_load_balancer_microk8s_conf" {
+#  metadata {
+#    name = "nginx-load-balancer-microk8s-conf"
+#    namespace = "ingress"
+#    annotations = {
+#      "kubectl.kubernetes.io/last-applied-configuration" = "{\"apiVersion\":\"v1\",\"kind\":\"ConfigMap\",\"metadata\":{\"annotations\":{},\"name\":\"nginx-load-balancer-microk8s-conf\",\"namespace\":\"ingress\"}}"
+#    }
+#  }
+#  data = {
+#    use-forwarded-headers = "true"
+#  }
+#}
+
+#locals {
+#  nginx_load_balancer_doc = file("${path.module}/k8s_yaml/nginx-load-balancer-microk8s-conf.yml")
+#}
+#
+#resource "kubernetes_manifest" "nginx_load_balancer_microk8s_conf" {
+#  manifest = yamldecode(local.nginx_load_balancer_doc)
+#  field_manager {
+#    force_conflicts = true
+#  }
+#}
