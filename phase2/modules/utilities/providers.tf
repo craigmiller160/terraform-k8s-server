a226acb7885c3551d97033863144078b1b5fa6e8
Craig Miller <craigmiller160@gmail.com>
2022-08-15 18:00:37 -0400

    Removing provider

--- phase2/modules/utilities/providers.tf
+++ phase2/modules/utilities/providers.tf
@@ -1,9 +1,5 @@
 terraform {
   required_providers {
-    docker = {
-      source  = "kreuzwerker/docker"
-      version = "2.20.0"
-    }
     kubernetes = {
       source = "hashicorp/kubernetes"
       version = "2.12.1"
