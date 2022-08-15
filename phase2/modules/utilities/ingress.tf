91c1a2515498d50b9c1139088987b2d0af2c71f7
Craig Miller <craigmiller160@gmail.com>
2022-08-14 20:13:59 -0400

    Cleanup

--- phase2/modules/utilities/ingress.tf
+++ phase2/modules/utilities/ingress.tf
@@ -30,45 +30,9 @@ resource "kubernetes_ingress_v1" "ingress" {
             }
           }
         }
+
+
       }
     }
   }
-#  spec {
-#    rule {
-#      http {
-#        path {
-#          path = "/nexus"
-#          path_type = "Prefix"
-#          backend {
-#            service_name = "nexus-service"
-#            service_port = 8081
-#          }
-#        }
-#      }
-#    }
-#  }
 }
-
-#resource "kubernetes_config_map" "nginx_load_balancer_microk8s_conf" {
-#  metadata {
-#    name = "nginx-load-balancer-microk8s-conf"
-#    namespace = "ingress"
-#    annotations = {
-#      "kubectl.kubernetes.io/last-applied-configuration" = "{\"apiVersion\":\"v1\",\"kind\":\"ConfigMap\",\"metadata\":{\"annotations\":{},\"name\":\"nginx-load-balancer-microk8s-conf\",\"namespace\":\"ingress\"}}"
-#    }
-#  }
-#  data = {
-#    use-forwarded-headers = "true"
-#  }
-#}
-
-#locals {
-#  nginx_load_balancer_doc = file("${path.module}/k8s_yaml/nginx-load-balancer-microk8s-conf.yml")
-#}
-#
-#resource "kubernetes_manifest" "nginx_load_balancer_microk8s_conf" {
-#  manifest = yamldecode(local.nginx_load_balancer_doc)
-#  field_manager {
-#    force_conflicts = true
-#  }
-#}
