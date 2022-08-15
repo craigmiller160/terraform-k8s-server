25e71f5748cb88608a19fc897675461bfa84b989
Craig Miller <craigmiller160@gmail.com>
2022-08-15 18:40:16 -0400

    More progress with 1password refactor

--- phase2/1password_provided.tf
+++ phase2/1password_provided.tf
@@ -12,6 +12,51 @@ resource "kubernetes_service_account" "onepassword_connect_operator_service_acco
   }
 }
 
+resource "kubernetes_cluster_role_binding" "onepassword_connect_operator_cluster_role_binding" {
+  metadata {
+    name = "onepassword-connect-operator-default"
+  }
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "ClusterRole"
+    name      = "onepassword-connect-operator"
+  }
+  subject {
+    kind = "ServiceAccount"
+    name = "onepassword-connect-operator"
+    namespace = "default"
+  }
+}
+
+resource "kubernetes_cluster_role" "onepassword_connect_operator_cluster_role" {
+  metadata {
+    name = "onepassword-connect-operator"
+  }
+  rule {
+    api_groups = [""]
+    verbs = [
+      "create",
+      "delete",
+      "get",
+      "list",
+      "patch",
+      "update",
+      "watch"
+    ]
+    resources = [
+      "pods",
+      "services",
+      "services/finalizers",
+      "endpoints",
+      "persistentvolumeclaims",
+      "events",
+      "configmaps",
+      "secrets",
+      "namespaces"
+    ]
+  }
+}
+
 resource "kubernetes_manifest" "onepassword_provided_service_account" {
   manifest = yamldecode(local.service_account_doc)
 }
