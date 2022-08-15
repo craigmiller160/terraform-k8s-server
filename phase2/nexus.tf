4334049c5f64a616b012e4227c821b0317c2e934
Craig Miller <craigmiller160@gmail.com>
2022-08-15 18:46:37 -0400

    Cleanup

--- phase2/nexus.tf
+++ phase2/nexus.tf
@@ -1,11 +1,3 @@
-locals {
-  nexus_all_docs = split("---",
-    replace(file("${path.module}/k8s_yaml/nexus.yml"), "%NEXUS_IMAGE%", var.nexus_image)
-  )
-  nexus_deployment_doc = local.nexus_all_docs.0
-  nexus_service_doc = local.nexus_all_docs.1
-}
-
 # TODO still needs major cleanup
 resource "kubernetes_deployment" "nexus" {
   depends_on = [
