b2f3142db0b3f4c1c09c04f6996d46d9c631f3b2
Craig Miller <craigmiller160@gmail.com>
2022-08-14 20:30:53 -0400

    Disabled ingress routes for now

--- phase2/modules/utilities/ingress.tf
+++ phase2/modules/utilities/ingress.tf
@@ -18,57 +18,57 @@ resource "kubernetes_ingress_v1" "ingress" {
           }
         }
 
-        path {
-          path = "/covid-19"
-          path_type = "Prefix"
-          backend {
-            service {
-              name = "covid-19-client-service"
-              port {
-                number = 443
-              }
-            }
-          }
-        }
-
-        path {
-          path = "/funcoast-hi"
-          path_type = "Prefix"
-          backend {
-            service {
-              name = "funcoast-hi-client-service"
-              port {
-                number = 443
-              }
-            }
-          }
-        }
-
-        path {
-          path = "/auth-management"
-          path_type = "Prefix"
-          backend {
-            service {
-              name = "auth-management-ui-service"
-              port {
-                number = 443
-              }
-            }
-          }
-        }
-
-        path {
-          path = "/video-manager"
-          path_type = "Prefix"
-          backend {
-            service {
-              name = "video-manager-client-service"
-              port {
-                number = 443
-              }
-            }
-          }
-        }
+#        path {
+#          path = "/covid-19"
+#          path_type = "Prefix"
+#          backend {
+#            service {
+#              name = "covid-19-client-service"
+#              port {
+#                number = 443
+#              }
+#            }
+#          }
+#        }
+#
+#        path {
+#          path = "/funcoast-hi"
+#          path_type = "Prefix"
+#          backend {
+#            service {
+#              name = "funcoast-hi-client-service"
+#              port {
+#                number = 443
+#              }
+#            }
+#          }
+#        }
+#
+#        path {
+#          path = "/auth-management"
+#          path_type = "Prefix"
+#          backend {
+#            service {
+#              name = "auth-management-ui-service"
+#              port {
+#                number = 443
+#              }
+#            }
+#          }
+#        }
+#
+#        path {
+#          path = "/video-manager"
+#          path_type = "Prefix"
+#          backend {
+#            service {
+#              name = "video-manager-client-service"
+#              port {
+#                number = 443
+#              }
+#            }
+#          }
+#        }
       }
     }
   }
