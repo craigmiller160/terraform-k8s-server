376bc292b7dd47958ff6f888d9f4b26d45eef887
Craig Miller <craigmiller160@gmail.com>
2022-08-16 15:36:13 -0400

    Working on new 1password module

---
+++ 1password/main.tf
@@ -0,0 +1,18 @@
+terraform {
+  backend "kubernetes" {
+    secret_suffix = "1password-state"
+    config_path   = "~/.kube/config"
+  }
+
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.12.1"
+    }
+  }
+}
+
+provider "kubernetes" {
+  config_path    = "~/.kube/config"
+  config_context = var.k8s_context
+}
---
+++ 1password/variables.tf
@@ -0,0 +1,11 @@
+variable "k8s_context" {
+  type = string
+}
+
+variable "onepassword_creds" {
+  type = string
+}
+
+variable "onepassword_token" {
+  type = string
+}
