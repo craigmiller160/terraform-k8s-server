dc28ca23a990827c64c9b75aea57b91986174ab2
Craig Miller <craigmiller160@gmail.com>
2022-08-17 16:11:06 -0400

    More improvements to all plans script

--- all_plans.sh
+++ all_plans.sh
@@ -5,6 +5,10 @@
 # $1 = env, $2 = directory
 function get_plan {
   plan=$(./$2/tf.sh $1 plan)
+  if [ $? -ne 0 ]; then
+    return 1
+  fi
+
   output=$(echo "$plan" | grep "Plan:")
   if [ ${#output} -gt 0 ]; then
     echo "  $output"
@@ -22,27 +26,39 @@ function validate_env {
   esac
 }
 
+function stop_if_error {
+  if [ $1 -ne 0 ]; then
+    exit 1
+  fi
+}
+
 # $1 = env
 function print_plans {
   echo "Getting plans for all modules"
 
   echo "k8s_custom_resources"
   get_plan $1 "k8s_custom_resources"
+  stop_if_error $?
 
   echo "1Password"
   get_plan $1 "1Password"
+  stop_if_error $?
 
   echo "databases"
   get_plan $1 "databases"
+  stop_if_error $?
 
   echo "nexus_deployment"
   get_plan $1 "nexus_deployment"
+  stop_if_error $?
 
   echo "nexus_config"
   get_plan $1 "nexus_config"
+  stop_if_error $?
 
   echo "ingress"
   get_plan $1 "ingress"
+  stop_if_error $?
 }
 
 validate_env $1
